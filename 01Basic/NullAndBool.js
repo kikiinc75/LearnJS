ae1a10d30278ee3ecb18d44c979a91ab6d023b31
wahyu iqbal efriansyah 47 <wahyuiqbal91@gmail.com>
2019-05-17 08:54:43 +0700

    doing stuff

---
+++ 01Basic/NullAndBool.js
@@ -0,0 +1,13 @@
+//temp is coming from website API
+
+var temp = 0
+
+//done some calculation
+
+console.log("Current temperature is: "+ temp)
+
+let actualMarks = 7
+
+let myGrade = (actualMarks == 10)
+
+console.log(myGrade)
